1139e1a6d66d9207e162a7951bdadc37a80ed65c
Andres <ricardo.gomez.restrepo@gmail.com>
2024-04-08 20:53:55 -0500

    updated cors middleware

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,6 +1,6 @@
 import cors from 'cors'
 
-const ORIGINS = ['*']
+const ORIGINS = ['http://localhost:5173/', 'http://localhost:5173']
 
 export const corsMiddleware = ({ acceptOrigin = ORIGINS } = {}) =>
   cors({
