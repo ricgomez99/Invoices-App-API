371f6bcd5d7708cb2d91117f9107556a37258919
Andres <andres@Andress-Laptop.local>
2024-07-24 16:07:48 -0500

    modified start command - add urls to cors middleware

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,6 +1,14 @@
 import cors from 'cors'
 
-const ORIGINS = ['http://localhost:5173/', 'http://localhost:5173']
+const ORIGINS = [
+  'http://localhost:5173/',
+  'http://localhost:5173',
+  'http://localhost:80',
+  'http://localhost:80/',
+  'http://localhost',
+  'http://localhost/',
+  'http://localhost:3000/auth/login',
+]
 
 export const corsMiddleware = ({ acceptOrigin = ORIGINS } = {}) =>
   cors({
